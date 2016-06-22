39ae2c8901759d23550ea0426dc2c8f1cb7ce229
root <root@localhost.localdomain>
2016-06-22 07:53:11 +0000

    update

--- phpspeed/library/appexception.php
+++ phpspeed/library/appexception.php
@@ -51,19 +51,15 @@ class appexception{
         switch($code){
             case 401 :
                 header('HTTP/1.1 401 Unauthorized');
-                header("Status Code: 401 Unauthorized");
                 break;
             case 403 :
                 header('HTTP/1.1 403 Forbidden');
-                header("Status Code: 403 Forbidden");
                 break;
             case 404 :
                 header('HTTP/1.1 404 Not Found');
-                header("Status Code: 404 Not Found");
                 break;
             case 500 :
                 header('HTTP/1.1 500 Internal Server Error');
-                header("Status Code: 500 Internal Server Error");
                 break;
         }
         exit;
